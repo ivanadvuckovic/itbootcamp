2632c75cfd6e541cfbb190b0739d35842b3e5988
ivanadvuckovic <ivanadvuckovic@gmail.com>
2021-06-21 10:24:39 +0200

    Added login page logic

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,5 +1,35 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class LoginPage extends BasicPage {
+
+	public LoginPage(WebDriver driver, JavascriptExecutor js) {
+		super(driver, js);
+	}
+	
+	public WebElement getLoginButton() {
+		return this.driver.findElement(By.linkText("Login"));
+	}
+	
+	public WebElement getUsername() {
+		return this.driver.findElement(By.xpath("//input[@name=\"username\"]"));
+	}
+	
+	public WebElement getPassword() {
+		return this.driver.findElement(By.xpath("//input[@name=\"password\"]"));
+	}
+	
+	public WebElement getLoginDialogButton() {
+		return this.driver.findElement(By.xpath("//input[@name=\"btn_submit\"]"));
+	}
+	
+	public void login(String username, String password) {
+		this.getLoginButton().click();
+		this.getUsername().sendKeys(username);
+		this.getPassword().sendKeys(password);
+	}
 }
